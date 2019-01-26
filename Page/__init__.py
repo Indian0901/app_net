5f68e70b8d68d8a359e7b5eaffedd929ee6267f8
Indian0901 <602317603@qq.com>
2019-01-26 22:02:28 +0800

    net

---
+++ Page/__init__.py
@@ -0,0 +1,7 @@
+from selenium.webdriver.common.by import By
+
+more_xpath = (By.XPATH,"//*[contains(@text,'更多')]")
+net_xpath = (By.XPATH,"//*[contains(@text,'移动网络')]")
+one_xpath = (By.XPATH,"//*[contains(@text,'首选')]")
+numtype_xpath = (By.XPATH,"//*[contains(@text,'3G')]")
+res_id = (By.ID,'android:id/summary')
---
+++ Page/page_set.py
@@ -0,0 +1,9 @@
+from Base.base_net import Base_net
+import Page
+
+class Page_set(Base_net):
+    def __init__(self,driver):
+        Base_net.__init__(self,driver)
+
+    def more_btn(self):
+        self.click_element(Page.more_xpath)
